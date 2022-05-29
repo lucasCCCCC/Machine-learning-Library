8d89f98b87aa59b8207f045d21cc22d97826c7ae
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-05-29 14:39:40 +0100

    Program now prints out which point belongs to each cluster

--- Unsupervised Learning/DBSCAN.py
+++ Unsupervised Learning/DBSCAN.py
@@ -12,7 +12,7 @@ class Dbscan:
         self.test_data = np.array(testData)
 
     def euclideanDistance(self, p1, p2):
-        return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+        return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
     def findNeighbourhoodPoints(self, x):
         neighbours = []
@@ -26,8 +26,7 @@ class Dbscan:
     def cluster(self):
         neighboursQueue = queue.Queue()
 
-        self.test_data = np.append(self.test_data,
-                                   np.array([[-1] * len(test_data)]).reshape(-1, 1), axis=1)
+        self.test_data = np.append(self.test_data, np.array([[-1] * len(test_data)]).reshape(-1, 1), axis=1)
 
         for i in range(len(self.test_data)):
 
@@ -68,8 +67,29 @@ class Dbscan:
                             neighboursQueue.put(i)
                             foundNeighbours.append(i)
 
+    def printClusters(self):
 
-test_data = np.array([[1, 1], [2, 2], [3, 4], [4, 4], [5, 5], [5, 6], [6, 5], [7, 7], [7, 6], [8, 8], [9, 7], [10, 11]])
+        clusters = {}
 
-dbscan = Dbscan(2, 1, test_data)
+        for x in self.test_data:
+            clusters.setdefault(str(x[2]), [])
+            coordinate = "(" + str(x[0]) + "," + str(x[1]) + ")"
+            clusters[str(x[2])].append(coordinate)
+
+        for cluster, point in clusters.items():
+            print("Points in cluster ", cluster, ":")
+            point = str(point).replace("[", "").replace("]", "").replace("'", "")
+            print(point)
+
+
+test_data = np.array([[0.9, 1], [2, 1], [2.5, 2], [1.2, 2], [5, 3.5], [5, 5.4], [4, 3.9],
+                      [5.5, 4], [5.1, 4.6], [4.1, 3.6], [4.6, 3.9],
+                      [8, 7], [5.5, 6], [8, 6.5], [7.5, 7.9], [7.5, 6.8]])
+
+
+epsilon = 2
+minPoints = 2
+
+dbscan = Dbscan(epsilon, minPoints, test_data)
 dbscan.cluster()
+dbscan.printClusters()
