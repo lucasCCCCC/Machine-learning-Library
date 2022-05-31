b18518344dd4eff98a25b6dd5bb88f9ccc796dd2
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-05-31 14:16:03 +0100

    update

--- Supervised Learning/NaiveBayes.py
+++ Supervised Learning/NaiveBayes.py
@@ -17,16 +17,16 @@ class NaiveBayes:
         for i in range(self.classes):
             X_c = x[y == i]
 
-            self.mean[str[i]] = np.mean(X_c, axis=0)
-            self.variance[str[i]] = np.var(X_c, axis=0)
-            self.prior[str[i]] = X_c.shape[0]/self.samples
+            self.mean[str(i)] = np.mean(X_c, axis=0)
+            self.variance[str(i)] = np.var(X_c, axis=0)
+            self.prior[str(i)] = X_c.shape[0]/self.samples
 
     def predictClass(self, x):
         probability = np.zeros((self.samples, self.classes))
 
         for i in range(self.classes):
-            prior = self.prior[str[i]]
-            prob = self.pdf(x, self.mean[str[i]], self.variance[str[i]])
+            prior = self.prior[str(i)]
+            prob = self.pdf(x, self.mean[str(i)], self.variance[str(i)])
             probability[:, i] = prob + np.log(prior)
 
         return np.argmax(probability, 1)
@@ -34,5 +34,5 @@ class NaiveBayes:
     def pdf(self, x, mean, sigma):
         CONSTANT = -self.attributes/2 * np.log(2*np.pi) - 0.5*np.sum(np.log(sigma + 1e-6))
         PROBABILITY = 0.5*np.sum(np.power(x - mean, 2) / (sigma + 1e-6), 1)
-        return (CONSTANT - PROBABILITY)
+        return CONSTANT - PROBABILITY
 
